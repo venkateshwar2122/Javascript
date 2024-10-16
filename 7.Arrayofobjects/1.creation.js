ad29da20426eb30e0c347827bb61ddaa04f06cb4
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-16 12:22:54 +0530

    Create 1.creation.js

---
+++ 7.Arrayofobjects/1.creation.js
@@ -0,0 +1,18 @@
+//array of objects
+let a = [{pid:101, name:"com", price:180},
+            {pid:102, name:"kb", price:200},
+            {pid:103, name:"mc", price:300}]
+
+console.log(a)
+
+//fetching object wise
+for(let i =0 ; i<a.length ; i++){
+    console.log(a[i])
+}
+
+//fetching property wise
+for(let i = 0; i<a.length ;i++){
+    console.log(a[i].pid)
+    console.log(a[i].name)
+    console.log(a[i].price)
+}
