e4f77098971e989dd4c50479778fa69529cbd8dd
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-16 12:26:13 +0530

    Create 1.fetching.js

---
+++ 8.Strings/1.fetching.js
@@ -0,0 +1,13 @@
+s1 = "java script"
+s2 = "react js"
+
+console.log(s1,s2)
+console.log(s1.length)
+console.log(s1[0])
+console.log(s1.charAt(0))
+
+// fetching
+
+for(let i = 0 ;i<s1.length ; i++){
+    console.log(s1[i])
+}
