93c5d53e5e8af03208da0d3dbbb27028f7c208f1
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-18 13:14:51 +0530

    Create 11.removing_leading_trailing_spaces.js

---
+++ 8.Strings/11.removing_leading_trailing_spaces.js
@@ -0,0 +1,8 @@
+// remove both leading and trailing spaces use trim()
+//remove only leading spaces use trimStart()
+//remove only trailing spaces use trimEnd()]
+
+let s = "     sd hs   dsd   s     "
+console.log(s.trim())
+console.log(s.trimStart())
+console.log(s.trimEnd())
