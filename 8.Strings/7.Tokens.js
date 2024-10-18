63336d8319dd87de9e5975f23c45c341bc97a91b
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-18 11:45:51 +0530

    Create 7.Tokens.js

---
+++ 8.Strings/7.Tokens.js
@@ -0,0 +1,13 @@
+let s = "hello world this is venkatesh"
+
+//sp[lit into characters
+console.log(s.split(""))   //no space between quotes
+
+//take "this" as delimiter
+console.log(s.split("this"))
+
+//split into substring
+console.log(s.split(" ")) //space between quotes
+
+//display just first 2 tokens
+console.log(s.split(" ",2))
