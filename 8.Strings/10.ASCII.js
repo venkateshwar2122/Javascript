771ce4644be27dedafd35a7487ff866046777385
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-18 13:14:01 +0530

    Create 10.ASCII.js

---
+++ 8.Strings/10.ASCII.js
@@ -0,0 +1,11 @@
+//ASCII a-z value is 97-122
+//ASCII A-Z value is 65-90
+//ASCII 0-9 value is 48-57
+
+let s ="heA2z"
+
+console.log(s.codePointAt(0))  // "h" ASCII value is 104
+console.log(s.codePointAt(1))
+console.log(s.codePointAt(2))
+console.log(s.codePointAt(3))
+console.log(s.codePointAt(4))
