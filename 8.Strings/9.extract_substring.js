722680eb18b0ee144c8c198ee0643ac4c1feda7a
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-18 13:13:17 +0530

    Create 9.extract_substring.js

---
+++ 8.Strings/9.extract_substring.js
@@ -0,0 +1,9 @@
+// to extract substring from main string
+//array.slice(startindex, endindex)
+//array.substring(startindex, endindex)
+//array.substr(index, length)
+
+let s = "hellotoall"
+console.log(s.slice(3,6))
+console.log(s.substring(3,6))
+console.log(s.substr(7,2))
