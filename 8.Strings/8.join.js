af79f8be64a7c9b4610f95894336bb912cc03c6c
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-18 13:12:33 +0530

    Create 8.join.js

---
+++ 8.Strings/8.join.js
@@ -0,0 +1,6 @@
+// convert array of strings into single string based on delimiter
+
+let s = ["html", "css", "javascript", "react", "express", "mongodb"]
+console.log(s.join("******")) // in join u have to give delimiter
+
+console.log(s.toString())  // tostring() default delimiter is comma
