263de0ef8d381858d12bdcb8d85d4f66fa25e9a9
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-20 18:43:37 +0530

    Update 1.delete_negative_number.js

--- 4.Array/4.1.Array Program/1.delete_negative_number.js
+++ 4.Array/4.1.Array Program/1.delete_negative_number.js
@@ -1,1 +1,30 @@
 //delete all negative number from given array
+
+let a = [1,2,-1,-3,55,-7]
+
+//deletion anamoly
+//using while loop
+let i=0.         // don't forget to initialise i before using in while loop
+while(i < a.length){
+    if(a[i]<0){
+        a.splice(i,1)
+    }
+    else{
+        i++
+    }
+}
+console.log(a)
+
+//deletion anamoly
+//using for loop
+for (let i=0 ; i<a.length;   ){   // note that i have not done i++
+    
+    if (a[i]<0){
+        a.splice(i,1)
+    }
+    else{
+        i++
+    }
+}
+
+console.log(a)
