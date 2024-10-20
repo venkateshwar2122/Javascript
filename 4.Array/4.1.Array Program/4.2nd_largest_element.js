fc65a0c9003c5105abd8c8b2968d1da00dbf28c0
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-20 20:50:56 +0530

    Update 4.2nd_largest_element.js

--- 4.Array/4.1.Array Program/4.2nd_largest_element.js
+++ 4.Array/4.1.Array Program/4.2nd_largest_element.js
@@ -1,1 +1,21 @@
+//find 2nd largest element in the array
 
+let a =[100,200,300,400,500]
+let max1 = max2 = a[0]
+
+//if array contain only one element
+//then take max1 = a[i] and max2= -1
+
+for(let i=0; i<a.length; i++){
+    
+    if(a[i] > max1){
+        max2 = max1  // remember sequence,first max2 = max1 then max1 = a[i]
+        max1 = a[i]
+        
+    }
+    else if(a[i]>max2 && a[i]!= max1){
+        max2 = a[i]
+    }
+}
+
+console.log(max2)
