0f4b2260d5730672d0270b014d7f322c0fd1e9d4
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-21 21:46:06 +0530

    Update 8.shift_negativenum_to_beginning.js

--- 4.Array/4.1.Array Program/8.shift_negativenum_to_beginning.js
+++ 4.Array/4.1.Array Program/8.shift_negativenum_to_beginning.js
@@ -1,1 +1,13 @@
 //shift all negative number to begnning of array
+//pg22
+
+let a = [1,-2,-3,4,5,-6,7,-8]
+
+let i = 0      // pointer to track non-negative intger
+for(let j =0 ; j<a.length ;j++){
+    if(a[j]<0){
+        a.splice(i, 0 ,a.splice(j,1)[0]) // here we are shifting negative integer to right, note we are not swapping
+        i++
+    }
+}
+console.log(a)
