714fa85fcd2cfc979c7f9c1176168d874ce2402f
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-21 21:58:52 +0530

    Create 6.array_to_string.js

---
+++ 4.Array/6.array_to_string.js
@@ -0,0 +1,4 @@
+let a = [10,20,30,40,20,20,50]
+console.log(a)
+console.log(a.toString())     //toString() method converts an array (or any object) into a
+                              // string where all the elements are concatenated and separated by commas.
