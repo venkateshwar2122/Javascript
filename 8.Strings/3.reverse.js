ae52bd5c961ce507cf729fe42354f8cfddb82936
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-21 23:28:50 +0530

    Update 3.reverse.js

--- 8.Strings/3.reverse.js
+++ 8.Strings/3.reverse.js
@@ -1,8 +1,14 @@
 let s = "java script"
 let r = ""
 
-for(let i = s.length-1;i>=0;i--){
-    r = r + s[i]
+//method1
+for(let i = s.length-1; i>=0; i--){           // decrement
+    r = r + s[i]                           // r at first place ,  s[i] at second place
 }
+console.log(r)
 
+//method2
+for(let i = 0 ; i<s.length ;i++){                //increment
+    r = s[i] + r                                 //s[i] at first place , r at second place
+}
 console.log(r)
