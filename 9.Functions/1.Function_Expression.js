517edc97019451c44abdaaca80252fe769c4c693
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-24 12:47:10 +0530

    Update 1.Function_Expression.js

--- 9.Functions/1.Function_Expression.js
+++ 9.Functions/1.Function_Expression.js
@@ -1,1 +1,10 @@
-..
+// function is assigned to a variable
+//A function expression is a way to define a function in JavaScript where the function is assigned to a variable.
+//Function expressions can be used in situations where you want to pass a function as an argument,
+//return a function from another function, or create closures.
+
+let add = function(a,b){
+    return a+b
+}
+
+console.log(add(4,5))
