6144ef97ab1c5e481943e3b6e20985abcfdcaedd
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-24 13:17:07 +0530

    Create 1.hoisting.js

---
+++ 9.Functions/9.2 Nested Function/1.hoisting.js
@@ -0,0 +1,12 @@
+console.log(add(10,20))              // fxn call
+
+function add(x,y){                  // fxn definition
+    return x+y
+}
+
+//no error even since is function is called before function definition
+//because of hoisting concept
+
+/*Hoisting is a JavaScript mechanism where variable and function declarations are moved to the top of 
+their containing scope during the compilation phase.This means that you can use variables and functions 
+before they are declared in the code.*/
