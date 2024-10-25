b744f26b833e18ec2a46b4ba2bc600db749fc9fa
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-25 12:43:20 +0530

    Create 3.MAP.js

---
+++ 9.Functions/9.3.Higher_order_function/3.MAP.js
@@ -0,0 +1,28 @@
+/* We will use MAP , to operate some functionality in each element of array */
+/* the fxnality can be like to divide every elemnet of array by 2 or find square of every element of array or etc*/
+/* note that MAP does not modify the source aray, the modification is done in new array
+
+
+/*
+we will use syntax1 mostly
+syntax1:
+
+updatedarray = arr.map((element, index, arr) => {
+                  state.1
+                  state.n
+                return updatedelement
+              })
+
+*/
+
+/*
+syntax2:
+
+let fun = (element, index, arr) => {
+                  state.1
+                  state.n
+                return updatedelement
+              }
+arr.map(fun)
+
+*/
