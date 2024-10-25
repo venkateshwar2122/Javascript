ddbca4493385dc1d0c4463283210e9e0c6b746a0
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-25 19:54:11 +0530

    Update 7.nth_prime_number.js

--- 9.Functions/9.1 Function program/7.nth_prime_number.js
+++ 9.Functions/9.1 Function program/7.nth_prime_number.js
@@ -1,26 +1,31 @@
-function isprime(n){
-    let c=0,i=2
-    while(true){
-        let f=true
-        if(n<=1){
-            f=false
-        }
-        else{
-            for(let j=2;j<i;j++){
-                if(i%j==0){
-                    f=false
-                    break
-                }
-            }
-            if(f){
-                c++
-                if(c==n){
-                    return i
-                }
-            }
-        }
-        i++
-  
+//find nth prime number
+//as we know prime number are :2,3,5,7,11,13,17,......
+// for n = 1 , output:2
+//for n = 2 , output:3
+//for n = 3, output:5
+//for n = 4 , output:7
+
+function isPrime(num) {
+    if (num <= 1) return false;
+
+    for (let i = 2; i < num; i++) {
+        if (num % i == 0) return false;
     }
+
+    return true;
 }
-console.log(isprime(5))
+
+let count = 0;
+let n = 5; // Change this to find the nth prime
+let k = 2; // Start from the first prime number
+
+while (true) {
+    if (isPrime(k)) {
+        count++;
+        if (count == n) {
+            console.log(k); // Output the nth prime number
+            break;
+        }
+    }
+    k++;
+}
