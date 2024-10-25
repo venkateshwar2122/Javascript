5c37b02198a8c2596400b7f6eabcba631b49029e
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-25 12:50:42 +0530

    Create 4.double_every_element.js

---
+++ 9.Functions/9.3.Higher_order_function/4.double_every_element.js
@@ -0,0 +1,12 @@
+// double every element in a given array using MAP
+
+let a = [2,6,4,8,9]
+let r = a.map((e) => e*2 )
+
+console.log(a)
+console.log(r)
+
+/* output : [ 2, 6, 4, 8, 9 ]
+[ 4, 12, 8, 16, 18 ] */
+
+// note that modification is done in new array and not source array
