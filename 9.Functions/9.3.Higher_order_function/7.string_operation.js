99d8a1d66d765d0e54b674a3b6b437eda1e65865
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-25 13:25:51 +0530

    Create 7.string_operation.js

---
+++ 9.Functions/9.3.Higher_order_function/7.string_operation.js
@@ -0,0 +1,11 @@
+// convert every string first letter as capital and all other letter in lower case in a given array of strings
+
+let a = ["heLLO", "gOOD" , "fiVe", "weLCOME"]
+
+let r = a.map((e)=> {
+
+        e = e[0].toUpperCase() + e.slice(1,e.length).toLowerCase()
+        return e 
+    } )
+
+console.log(r)
