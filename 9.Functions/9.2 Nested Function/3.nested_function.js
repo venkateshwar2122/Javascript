9fe22008425d897ccde490be14e1339052754053
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-25 11:58:22 +0530

    Create 3.nested_function.js

---
+++ 9.Functions/9.2 Nested Function/3.nested_function.js
@@ -0,0 +1,14 @@
+// nested function progran
+
+function ofun(){
+    let a=10
+    function ifun()
+    {
+        let b =20
+        console.log(a,b)
+    }
+    ifun()
+}
+ofun()
+
+// output: 10 20
