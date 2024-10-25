ba77c5cdd335d5b32759943092e86810c2f95221
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-25 18:51:28 +0530

    Update 3.ispalindrome.js

--- 9.Functions/9.1 Function program/3.ispalindrome.js
+++ 9.Functions/9.1 Function program/3.ispalindrome.js
@@ -1,9 +1,9 @@
 function ispalindrome(n){
     
-    let t = n ,r = 0
+    let t = n ,r = 0   // outside whileloop declaration
     while(n!=0){
-    r = r*10 + n%10
-    n = parseInt(n/10)
+    r = r*10 + n%10           //***
+    n = parseInt(n/10)       //***
     }
     return t==r
 }
