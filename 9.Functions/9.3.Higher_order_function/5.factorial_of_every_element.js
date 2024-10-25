7dd0b58278fa680256055cff8f474216e8cecd46
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-25 13:04:26 +0530

    Create 5.factorial_of_every_element.js

---
+++ 9.Functions/9.3.Higher_order_function/5.factorial_of_every_element.js
@@ -0,0 +1,34 @@
+/* understand how iteration is happening in MAP:
+
+let a = [7,8,4,5,6]
+let r = a.map((e) => {
+         console.log(e)
+         })
+console.log(r)
+
+*/
+
+/* OUTPUT:
+7
+8
+4
+5
+6
+[undefined, undefined, undefined, undefined, undefined ]
+*/
+
+// program to calculate factorial of every elemnt in array
+
+let a = [7, 8 ,4,5,6]
+let r = a.map((e)=>{
+        let f = 1
+        for(let i = 1; i<=e ;i++){
+            f=f * i
+        }
+        return f
+})
+console.log(r)
+
+/*  OUTPUT:
+[ 5040, 40320, 24, 120, 720 ]
+ */
