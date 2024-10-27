91ea466af78b30fa818c96988370c8637dc3c54e
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-27 22:29:12 +0530

    Update 17.EVERV().js

--- 9.Functions/9.3.Higher_order_function/17.EVERV().js
+++ 9.Functions/9.3.Higher_order_function/17.EVERV().js
@@ -3,10 +3,64 @@
 /* The every() method is a powerful tool in JavaScript that allows you to determine if every element in an 
 array meets a specific condition. It takes a callback function as an argument and returns a 
   boolean value: true if all elements pass the test, false otherwise.
-*/ syntax
+*/ syntax:-
 let r = array.every(element, index, array)=>{
         return condition
        })
+
+/* check how iteration is working in every()
+example.1
+let numbers = [2, 4, 6, 8, 10];
+
+let r = numbers.every((ele, index, arr) => {
+ console.log(ele)
+ console.log(index)
+ console.log(arr)
+ return ele<0                // condition is ele is -ve or not
+});
+
+console.log(r); 
+
+output:
+2
+0
+[ 2, 4, 6, 8, 10 ]
+false
+
+
+example2.
+let numbers = [2, 4, 6, 8, 10];
+
+let r = numbers.every((ele, index, arr) => {
+ console.log(ele)
+ console.log(index)
+ console.log(arr)
+ return ele>0              // condition is ele is +ve or not
+});
+console.log(r); 
+
+
+output:
+2
+0
+[ 2, 4, 6, 8, 10 ]
+4
+1
+[ 2, 4, 6, 8, 10 ]
+6
+2
+[ 2, 4, 6, 8, 10 ]
+8
+3
+[ 2, 4, 6, 8, 10 ]
+10
+4
+[ 2, 4, 6, 8, 10 ]
+true
+
+*/
+
+
 
 //que1. check all elemn=ent in array are even or not
 
