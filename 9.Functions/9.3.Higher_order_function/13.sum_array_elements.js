b7873f462cb2829023a4c5f109cde9b5ab64d37e
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-27 19:52:09 +0530

    Create 13.sum_array_elements.js

---
+++ 9.Functions/9.3.Higher_order_function/13.sum_array_elements.js
@@ -0,0 +1,12 @@
+//sum all the elements of the array
+
+let a = [10,20,30,40,50]
+
+let r1 = a.reduce((acc,e,index,arr)=>{
+        
+       return acc+e
+       })
+console.log(r1)
+
+//output :150
+
