eeec17996a814ad9c0bce9640cadf09c99610270
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-27 21:47:20 +0530

    Update 15.positive_number_count.js

--- 9.Functions/9.3.Higher_order_function/15.positive_number_count.js
+++ 9.Functions/9.3.Higher_order_function/15.positive_number_count.js
@@ -1,3 +1,30 @@
+
+/* EXCEPTION: if u initialise the accumulator value with any value , then "ele" and "index" by default WILL NOT START from
+2nd element,it will start from first element itself
+ex
+let a = [10,20,30,40,50]
+let r = a.reduce((acc,ele,index)=>{
+        console.log(ele)
+        console.log(index)
+        },128)              //initialised acc value to "128"(any random value)
+        
+console.log(r)
+
+output:
+10        // ele1 is the first element instead of second element
+0         //index1 is the index of first element instead of second element
+20        //ele2
+1         //index2
+30
+2
+40
+3
+50
+4
+128    // r value which is equal to "return acc"
+*/
+
+
 //using reduce() find count of positive number in array
 
 let a = [-10,-20,30,40,60,-50]
