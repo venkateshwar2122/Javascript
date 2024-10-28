e973e5de3d2915c6841012b98192ce839d713b95
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-28 12:14:51 +0530

    Create 20.forEach().js

---
+++ 9.Functions/9.3.Higher_order_function/20.forEach().js
@@ -0,0 +1,16 @@
+// to iterate in same array
+//note map returns updated array whereas, foreach doesn't resturn any updated array
+// like for loop
+
+let a = [10,20,30,40]
+
+a.forEach((e)=>{
+    console.log(e)
+})
+
+/*output:
+10
+20
+30
+40
+*/
