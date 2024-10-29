c120393ceedd8e3b5c8d8e8576f3aaabb9b4e0d1
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-29 12:23:54 +0530

    Create 3.power.js

---
+++ 9.Functions/9.4 Recursion/3.power.js
@@ -0,0 +1,19 @@
+//define a recursive function to calculate a power b (a**b)
+/* recursive function
+    1) 1, b=0
+    2) a*f(a,b-1) , b>0
+
+    */
+
+function fun(a,b){
+    if(b==0){
+        return 1
+    }
+    else{
+
+        return a*fun(a,b-1)
+
+    }
+}
+
+console.log(fun(2,3))
