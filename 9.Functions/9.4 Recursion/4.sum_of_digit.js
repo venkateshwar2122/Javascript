d24dca3f0cff19ef67eb52e0b69826482a69a7f8
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-10-29 12:43:51 +0530

    Create 4.sum_of_digit.js

---
+++ 9.Functions/9.4 Recursion/4.sum_of_digit.js
@@ -0,0 +1,17 @@
+//sum of individual digit of given number
+
+/* recursive functon
+  1)0  , n=0
+  2)n%10 + fun(parseint(n/10))    , n>0
+  */
+
+function fun(n){
+    if(n==0){
+        return 0
+    }
+    else{
+        return n%10 + fun(parseInt(n/10))
+    }
+}
+
+console.log(fun(123))
