c1b8bcedc5d15e9d228aff1c3be29dcf5b3fb2e0
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-11-04 13:19:27 +0530

    Create 7.prog2.js

---
+++ 9.Functions/9.5 Scope/7.prog2.js
@@ -0,0 +1,20 @@
+// var variables can be redeclared in the same scope but let and const variables cannot be redeclared 
+
+/* allowed
+
+var x = 10
+var x = 30
+
+*/
+
+
+
+/* not allowed
+
+let x =10
+let x=30
+
+const x = 10
+const x = 30
+
+*/
