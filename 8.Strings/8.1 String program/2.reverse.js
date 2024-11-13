c73d8cf016349bf1649ac8aeb6cee5bdfa494be4
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-11-13 20:36:33 +0530

    Create 2.reverse.js

---
+++ 8.Strings/8.1 String program/2.reverse.js
@@ -0,0 +1,16 @@
+//pg48
+
+let s = "java script"
+let r = ""                                    /*  take r as string datatyoe */
+
+//method1
+for(let i = s.length-1; i>=0; i--){           // decrement
+    r = r + s[i]                           // r at first place ,  s[i] at second place
+}
+console.log(r)
+
+//method2
+for(let i = 0 ; i<s.length ;i++){                //increment
+    r = s[i] + r                                 //s[i] at first place , r at second place
+}
+console.log(r)
