f4ea4eb1aebe9d71286ddf65769361cbcf1965a2
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-11-13 21:09:13 +0530

    Update 3.reverse_word_in_string.js

--- 8.Strings/8.1 String program/3.reverse_word_in_string.js
+++ 8.Strings/8.1 String program/3.reverse_word_in_string.js
@@ -2,12 +2,31 @@
 //reverse every word in a given string in place
 
 //sir method
+let s = "abc 1234 cabc abcd"
+s = s.split(" ")     /*here split is dividing string according to space(" ")and stores into an array as a result each word of string is now an array element */
+let r =""          /* R IS STRING DATATYPE */
+
+for (let i=0;i<s.length;i++){
+
+
+    for(let j=0;j<s[i].length;j++){
+        
+         r = s[i][j]+ r      /*  r= S[I][J] +R  */  
+    }
+    s[i]=r
+    r = ""   /* reinitialising r */
+   
+}
+console.log(s.join())          /* join() converts array back to string and join array elements by commma)  */
 
 
 
-//my method
+
+
+
+//my method 1
 let s = "ab bcav 1234 cd da"
-s = s.split(" ")
+s = s.split(" ")   /*here split is dividing string according to space(" ")and stores into an array as a result each word of string is now an array element */
 console.log(s)
 
 for(let i = 0; i<s.length ; i++){
@@ -18,4 +37,23 @@ for(let i = 0; i<s.length ; i++){
    }
    s[i] = r
 }
-console.log(s)
+console.log(s.join())     /* join() converts array back to string and join array elements by commma)  */
+
+
+
+//My method 2
+let s = "abc 1234 cabc abcd"
+ s = s.split(" ")   /*here split is dividing string according to space(" ")and stores into an array as a result each word of string is now an array element */
+let r =" "
+
+for (let i=0;i<s.length;i++){
+    for(let j=0;j<s[i].length;j++){
+        
+         r = s[i][j]+ r                /*  r= S[I][J] + r */  
+    }
+    s[i]=r
+    r=""
+   
+}
+console.log(s.join())   /* join() converts array back to string and join array elements by commma)  */
+
