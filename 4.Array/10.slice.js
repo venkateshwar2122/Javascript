c2a7017fdd320a8197d9572debf1097decd37939
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-12-27 19:30:21 +0530

    Update 10.slice.js

--- 4.Array/10.slice.js
+++ 4.Array/10.slice.js
@@ -7,6 +7,6 @@ let a = [10,20,30,40,50]
 console.log(a.slice(2))  // (start index)
                          // output [30,40,50]
 
-console.log(a.slice(1,3))  // (startindex , endindex-1)
+console.log(a.slice(1,3))  // (startindex , endindex+1)
                            // output [20,30] and not [20,30,40]
                            // its taking endindex-1 value
