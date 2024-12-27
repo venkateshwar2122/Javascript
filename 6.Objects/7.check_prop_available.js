26ae486bdf3e85005eca391e057623d149b86554
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-12-27 23:09:58 +0530

    Update 7.check_prop_available.js

--- 6.Objects/7.check_prop_available.js
+++ 6.Objects/7.check_prop_available.js
@@ -3,7 +3,7 @@
 let obj = {sid:"101", name:"roshan", marks:100}
 
 //checking that "place" property is available in object or not
-console.log(obj.place!=undefined)
+console.log(obj.place!=undefined)                                        //false
 
 //checking that "name" property is available in object or not
-console.log(obj.name!=undefined)
+console.log(obj.name!=undefined)                                        //true
