aaa0d0d85683e85f399871c11b65854a7b7eb5fe
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-12-27 19:03:07 +0530

    Update 2.Adding_at_end.js

--- 4.Array/2.Adding_at_end.js
+++ 4.Array/2.Adding_at_end.js
@@ -10,17 +10,18 @@ console.log(a)
 
 let b = [1,2,3]
 b[10] = 2000
-console.log(b)
+console.log(b)     // [ 1, 2, 3, <7 empty items>, 2000 ]
+
 
 //old value replace with new value
 
 let c = [1,2,3]
 c[1]=3000
-console.log(c)
+console.log(c)   //[ 1, 3000, 3 ]
 
 //SPREAD OPEREATOR(...)
 
 let d = [34,35,36]
 d = [...d , 4000]
-console.log(d)
+console.log(d)   // [ 34, 35, 36, 4000 ]
 
