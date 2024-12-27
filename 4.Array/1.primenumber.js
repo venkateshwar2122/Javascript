fce57073708f035a6060bd2045ea3ae1c1e5276b
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-12-27 18:59:27 +0530

    Update 1.primenumber.js

--- 4.Array/1.primenumber.js
+++ 4.Array/1.primenumber.js
@@ -1,7 +1,7 @@
 let a = [2,3,4,5,6,7,8,9]
 for(let e of a){
     let f = true
-    for(let i=2; i<e ;i++){
+    for(let i=2; i<e ;i++){              // agar 2 se e ki bich koi ek bhi factor aaye(means e%i==0) then its not prime..there should be one factor thats itself only(we skipped 1 that why starting with 2)
         if(e%i==0){
             f = false
             break
