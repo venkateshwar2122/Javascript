42dc2e819b9e8c6e775360c9f928d894491a26aa
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-12-27 19:21:49 +0530

    Update 5.searching_elements.js

--- 4.Array/5.searching_elements.js
+++ 4.Array/5.searching_elements.js
@@ -1,5 +1,8 @@
-let a = [101,201,301,401,501,601,701,801,901,201]
+let a = [101,201,301,401,501,601,501,801,901,201,501,567]
 
 console.log(a.includes(50))  //false
-console.log(a.indexOf(501)) // 4 
+console.log(a.includes(101))  //true
+console.log(a.includes(101,3))    //false , beacuse it starts checking from 3rd index onwards
+console.log(a.indexOf(501)) // 4             // it gives index of first occurence
+console.log(a.lastIndexOf(501)) //  10        // it gives index of last occurence
 console.log(a.includes(201,4)) //true
