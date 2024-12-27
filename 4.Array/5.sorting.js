f9d331920e2bb3b0ddfe55e7263b24b23baa5baf
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-12-27 19:25:33 +0530

    Update 5.sorting.js

--- 4.Array/5.sorting.js
+++ 4.Array/5.sorting.js
@@ -1,8 +1,8 @@
 //sorting
 
 let a = [10,20,30,40,20,20,50]
-console.log(a.sort)  //it does string based sorting, therefore don't use this
+console.log(a.sort)  //it does string based sorting, therefore don't use this 
 
-console.log(a.sort((x,y)=> x-y))  //sort in ascending order, note:sort has 2 brackets
+console.log(a.sort((x,y)=> x-y))  //sort in ascending order, note:sort has 2 brackets //[10,20,20,20,30,40,50]
 
-console.log(a.sort((x,y)=>y-x))   //sort in descending order
+console.log(a.sort((x,y)=>y-x))   //sort in descending order//[50,40,30,20,20,20,10]
