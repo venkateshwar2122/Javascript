57ec6e5d92e9ce924c1652073a6db88ff7dbe475
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2024-12-28 09:51:02 +0530

    Update 1.accessing_data.js

--- 6.Objects/1.accessing_data.js
+++ 6.Objects/1.accessing_data.js
@@ -6,6 +6,6 @@ console.log(obj.name, obj.age, obj.marks) //aman 20 85
 console.log(obj['sid'], obj['name'], obj['age']) //101 aman 20    // ITS LIKE STRUCTURE DATATYPE THAT I STUDIED IN C LANGUAGE
 
 //method 3 to access data using for loop
-for (let p in obj){
+for (let p in obj){                 // if u go through structure datatype concept of c language then here "p" is working like index
     console.log(obj[p]) // p is variable, output is 101 aman 20 cse 85
 }
