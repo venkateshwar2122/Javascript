37406bf6d0d724da5d4ccaa83a080e20ed348bbb
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-03-18 19:31:39 +0530

    Update 2.HOF.js

--- 9.Functions/9.3.Higher_order_function/2.HOF.js
+++ 9.Functions/9.3.Higher_order_function/2.HOF.js
@@ -15,7 +15,7 @@ let area_sq = (s)=> {
 let area_circle =(r)=>{
     return 3.14*r*r
 }
-let area_cube=(s)=>{
+let vol_cube=(s)=>{
     return s**3
 }
 
@@ -31,7 +31,7 @@ function HOF(a,fun){
 
 HOF([3,7,3,4,8] , area_sq)                        //fxn call
 HOF([4.78, 5.61, 9.8, 5.1], area_circle)          //fxn call
-HOF([4,8,6,3,2],area_cube)                        //fxn call
+HOF([4,8,6,3,2],vol_cube)                        //fxn call
 
 /* OUTPUT: [ 9, 49, 9, 16, 64 ]
 [ 71.74397600000002, 98.82239400000002, 301.5656, 81.67139999999999 ]
