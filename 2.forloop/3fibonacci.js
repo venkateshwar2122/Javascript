6cf9f9e19f23fa52a9c7898b31e4949a4c657ea4
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-04-16 23:06:25 +0530

    Update 3fibonacci.js

--- 2.forloop/3fibonacci.js
+++ 2.forloop/3fibonacci.js
@@ -5,11 +5,42 @@
 
 */
 
-let a =-1, b = 1, c = 0
+let a =-1, b = 1, sum = 0
 let n =12
 for(i =1 ; i<=n ;i++){
-    c=a+b
-    console.log(c)
+    sum=a+b
+    console.log(sum)
     a = b
-    b = c
+    b = sum
 }
+
+
+//    MY CODE :-
+
+let a= 0, b=1
+let n=10
+let sum=0
+
+console.log(a)
+console.log(b)
+
+for(i=3;i<=10;i++){          /* STARTING WITH i=3, because 0 and 1 already printed */
+    sum=a+b
+    console.log(sum)
+    a=b
+    b=sum
+    
+}
+
+/* output:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+*/
