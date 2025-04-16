7fe39462d71cf84d9b107a3aef9011e552970288
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-04-16 22:43:34 +0530

    Update 1evenodd.js

--- 2.forloop/1evenodd.js
+++ 2.forloop/1evenodd.js
@@ -20,4 +20,15 @@ for (let i = 2; i <= n && i % 2 == 0; i+=2) {
     i+=2
 }
 
+// MY CODE
+
+let n = 10;
+for (let i = 0; i<=n; i++) {
+    if(i%2==0){
+        console.log(i)
+    }
+}
+
+
+NOTE: 0 is also an even number
 
