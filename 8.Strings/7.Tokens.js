f9d61f2b48ce69534f56ab61dbb3662f28ecb4f9
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-04-18 11:43:17 +0530

    Update 7.Tokens.js

--- 8.Strings/7.Tokens.js
+++ 8.Strings/7.Tokens.js
@@ -3,11 +3,26 @@ let s = "hello world this is venkatesh"
 //sp[lit into characters
 console.log(s.split(""))   //no space between quotes
 
+/* o/p : ["h", "e", "l", "l", "o", " ", "w", "o", "r", "l", "d", " ", "t", "h", "i", "s", " ", "i", "s", " ", "v", "e", "n", "k", "a", "t", "e", "s", "h"]
+*/
+
 //take "this" as delimiter
 console.log(s.split("this"))
+
+/* o/p:
+["hello world ", " is venkatesh"]
+*/
 
 //split into substring
 console.log(s.split(" ")) //space between quotes
 
+/*o/p:
+["hello", "world", "this", "is", "venkatesh"]
+*/
+
 //display just first 2 tokens
 console.log(s.split(" ",2))
+
+/*o/p:
+["hello", "world"]
+*/
