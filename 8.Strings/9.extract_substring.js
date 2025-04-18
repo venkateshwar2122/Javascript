4452d68011374dca47e6d45dcf340fa5f2314315
Venkateshwar <98686991+venkateshwar2122@users.noreply.github.com>
2025-04-18 11:51:18 +0530

    Update 9.extract_substring.js

--- 8.Strings/9.extract_substring.js
+++ 8.Strings/9.extract_substring.js
@@ -1,9 +1,9 @@
 // to extract substring from main string
-//array.slice(startindex, endindex)
-//array.substring(startindex, endindex)
+//array.slice(startindex, endindex,-1)
+//array.substring(startindex, endindex-1)
 //array.substr(index, length)
 
 let s = "hellotoall"
-console.log(s.slice(3,6))
-console.log(s.substring(3,6))
-console.log(s.substr(7,2))
+console.log(s.slice(3,6))      // lot
+console.log(s.substring(3,6)) // lot
+console.log(s.substr(7,2))    // al
